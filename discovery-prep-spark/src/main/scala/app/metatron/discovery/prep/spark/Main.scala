548aafe753e41c9f36c1805162d1404e76b616e1
Kim, Joo-Ho <teamsprint@gmail.com>
2018-09-06 00:22:11 +0900

    Parser works!

--- discovery-prep-spark/src/main/scala/app/metatron/discovery/prep/spark/Main.scala
+++ discovery-prep-spark/src/main/scala/app/metatron/discovery/prep/spark/Main.scala
@@ -1,10 +1,14 @@
 package app.metatron.discovery.prep.spark
 
+import app.metatron.discovery.prep.parser.preparation.RuleVisitorParser
 import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().config("spark.master", "local").getOrCreate()
     println("Hello, Spark!")
+
+    val rule = new RuleVisitorParser().parse("rename col: x to: y")
+    println(rule)
   }
 }
